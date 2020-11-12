849142f8c07a1cb3acdc36e76ade99e62356c8bc
Magda Barthofer <magda.barthofer@gmail.com>
2020-11-12 12:53:14 +0100

    added model validations

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
+  validates :ingredient, presence: true
+
+end
