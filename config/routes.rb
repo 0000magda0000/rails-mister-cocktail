36751e0812f21b2cf902baf21b71d2d6c2705291
Magda Barthofer <magda.barthofer@gmail.com>
2021-01-30 14:54:04 +0100

    7 crud action without only in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "cocktails#index"
-  resources :cocktails, only: [:show, :index, :create, :new, :edit, :update, :destroy] do
+  resources :cocktails do
     resources :ingredients, only: [:create]
     resources :doses, only: [:create, :new, :update]
   end
