f41cd135bcf2c83abe1d14a5302d74fab44917cd
Magda Barthofer <magda.barthofer@gmail.com>
2021-01-31 22:13:21 +0100

    set default image for cocktail

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,11 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   accepts_nested_attributes_for :ingredients, :doses
   has_one_attached :photo
+  after_commit :add_default_photo, on: [:create, :update]
+
+  def add_default_photo
+    unless photo.attached?
+      self.photo.attach(io: File.open(Rails.root.join("app", "assets", "images", "cocktail.jpg")), filename: 'cocktail.jpg', content_type: "image/jpg")
+    end
+  end
 end
